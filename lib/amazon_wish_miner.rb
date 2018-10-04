abc27692d141c2e9d5fb9b6f1b74dfc9ecf476a9
avtrujillo <alexandervtrujillo@gmail.com>
2018-10-03 21:11:58 -0500

    added AmazonWishList class with get_wishlist method

--- lib/amazon_wish_miner.rb
+++ lib/amazon_wish_miner.rb
@@ -1,1 +1,15 @@
-puts 'hello world'
+class AmazonWishList
+
+  REVEAL_OPTIONS = [:all, :purchased, :unpurchased].freeze
+  SORT_OPTIONS = [:date_added, :title, :price_high, :price_low, :date_updated, :priority].freeze
+
+  def initialize
+  end
+
+  def self.get_wishlist(amazon_user_id, tld, reveal, sort)
+    url = "http://www.amazon.#{tld}/registry/wishlist/#{amazon_user_id}"
+    query_string = "?#{reveal}&#{sort}&layout=standard"
+    first_page = HTTParty.get(url + query_string)
+  end
+
+end
