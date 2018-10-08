53f83be66976a3643cf54e2af923904cb94d7619
avtrujillo <alexandervtrujillo@gmail.com>
2018-10-08 15:55:25 -0500

    fixed requires in main file

--- lib/amazon_wish_miner.rb
+++ lib/amazon_wish_miner.rb
@@ -1,2 +1,4 @@
-require 'amazon_wish_miner/amazon_wish_list.rb'
-require 'amazon_wish_miner/amazon_wish.rb'
+require 'nokogiri'
+require 'httparty'
+require_relative 'amazon_wish_miner/amazon_wish_list.rb'
+require_relative 'amazon_wish_miner/amazon_wish.rb'
