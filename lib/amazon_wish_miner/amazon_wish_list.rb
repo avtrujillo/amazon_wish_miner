ac61f15a42a04c17ead5c07ff6e85482eab9a869
avtrujillo <alexandervtrujillo@gmail.com>
2018-10-08 15:38:33 -0500

    rearranged file tree

---
+++ lib/amazon_wish_miner/amazon_wish_list.rb
@@ -0,0 +1,20 @@
+class AmazonWishList
+
+  REVEAL_OPTIONS = [:all, :purchased, :unpurchased].freeze
+  SORT_OPTIONS = {date_added: "date-added", title: 'universal-title',
+    price_high: 'universal-price-desc', price_low: 'universal-price',
+    date_updated: 'last-updated', priority: 'priority'}.freeze
+
+  def initialize
+  end
+
+  def self.get_wishlist(amazon_user_id, reveal = :all, sort = :date_added, tld = 'com')
+    raise "invalid reveal" unless REVEAL_OPTIONS.include?(reveal)
+    sort_string = SORT_OPTIONS[sort]
+    raise "invalid sort" unless sort_string
+    url = "http://www.amazon.#{tld}/registry/wishlist/#{amazon_user_id}"
+    query_string = "?reveal=#{reveal.to_s}&sort=#{sort_string}&layout=standard"
+    first_page = HTTParty.get(url + query_string)
+  end
+
+end
