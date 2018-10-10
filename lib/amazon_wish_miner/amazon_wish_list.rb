467d369804a8e3b288bfbe9c3c6c24b50c1b8cd6
avtrujillo <alexandervtrujillo@gmail.com>
2018-10-10 18:17:19 -0500

    modified gitignore and changed the name of amazon_user_id to amazon_list_id

--- lib/amazon_wish_miner/amazon_wish_list.rb
+++ lib/amazon_wish_miner/amazon_wish_list.rb
@@ -8,11 +8,11 @@ class AmazonWishList
   def initialize
   end
 
-  def self.get_wishlist(amazon_user_id, reveal = :all, sort = :date_added, tld = 'com')
+  def self.get_wishlist(amazon_list_id, reveal = :all, sort = :date_added, tld = 'com')
     raise "invalid reveal" unless REVEAL_OPTIONS.include?(reveal)
     sort_string = SORT_OPTIONS[sort]
     raise "invalid sort" unless sort_string
-    url = "http://www.amazon.#{tld}/registry/wishlist/#{amazon_user_id}"
+    url = "http://www.amazon.#{tld}/registry/wishlist/#{amazon_list_id}"
     query_string = "?reveal=#{reveal.to_s}&sort=#{sort_string}&layout=standard"
     first_page = RestClient.get(url + query_string)
   end
