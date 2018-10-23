340d616c565f8e25fd1f207d00562fcf922e5747
avtrujillo <alexandervtrujillo@gmail.com>
2018-10-22 22:31:53 -0500

    can now get titles and urls to initialize AmazonWish objects

--- lib/amazon_wish_miner/amazon_wish.rb
+++ lib/amazon_wish_miner/amazon_wish.rb
@@ -1,5 +1,12 @@
 class AmazonWish
 
+  attr_reader: :title, :id
+
+  def initialize(id, title)
+    @title = title
+    @id = id
+  end
+
   def self.parse_wishes_from_pages(page_responses)
     list_items = self.list_items_from_response(page_responses)
     wish_ids = self.draps_from_list_items(list_items)
@@ -34,7 +41,27 @@ class AmazonWish
   # parsing item info from the item's own url rather than from the wishlist
   #=> means that we can reuse the method below to scrape item info
 
-  def self.wishes_from_ids
+  def self.wishes_from_ids(ids)
+    ids.map do |id|
+      self.item_from_id(id)
+    end
+  end
+
+  def self.item_from_id(id)
+    item_url = 'https://www.amazon.com/dp/' + id
+    response = RestClient.get(item_url)
+    page = Nokogiri::HTML(response)
+    title = page.css('span[id$="roductTitle"]') # not a typo, css selectors are
+    #=> case sensetive, and we need to capture e.g. both "productTitle" and "ebookProductTitle"
+    # price = page.css('priceblock_ourprice')
+    # TODO: parse prices
+    # description = parse_feature_bullets(page.css('div#feature-bullets'))
+    # TODO: get description parsing to work for different types of items
+    AmazonWish.new(id, title)
+  end
+
+  def self.parse_feature_bullets(feature_bullets_div)
+    bullets = feature_bullets_div.css('ul li')
   end
 
 end
